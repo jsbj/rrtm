d36d6c5c26f0b85bfcd7dcf7905d4355ed70669a
Jonah Bloch-Johnson <me@jonah.org>
2013-09-26 13:33:55 -0400

    checkboxes now

--- html/js/rrtm.js
+++ html/js/rrtm.js
@@ -209,9 +209,36 @@ updateOutput = function() {
     })
 }
 
+
+
 // =========
 // = Input =
 // =========
+
+
+var inputList = [
+    {nonSurfaceKey: 'Tbound', surfaceKey: 'Ts', min: 150, max: 350, label: 'Temperature (K)', on: true},
+    {nonSurfaceKey: 'rh', max: 100., label: 'Relative humidity (%)'},
+    {nonSurfaceKey: 'co2', max: 3000, label: 'CO2 (ppm)', double: 1, on: true},
+    {nonSurfaceKey: 'ch4', max: 10000, label: 'CH4 (ppb)', double: 2},
+    {nonSurfaceKey: 'n2o', max: 1000, label: 'N2O (ppb)', double: 1},
+    {nonSurfaceKey: 'o3', max: 50, label: 'O3 (ppm)', double: 2},
+    {nonSurfaceKey: 'cfc11', max: 1000, label: 'CFC-11 (ppt)', double: 1},
+    {nonSurfaceKey: 'cfc12', max: 1000, label: 'CFC-12 (ppt)', double: 2},
+    {nonSurfaceKey: 'cfc22', max: 1000, label: 'CFC-22 (ppt)', double: 1},
+    {nonSurfaceKey: 'ccl4', max: 50, label: 'CCl4 (ppt)', double: 2},
+    {nonSurfaceKey: 'cldf', max: 1, label: 'Cloud fraction'},
+    {nonSurfaceKey: 'clwp', max: 30, label: 'In-cloud liquid water path (g/m2)'},
+    {nonSurfaceKey: 'r_liq', max: 100, label: 'Cloud water drop radius (10^-6 m)'},
+    {nonSurfaceKey: 'ciwp', max: 30, label: 'In-cloud ice water path (g/m2)'},
+    {nonSurfaceKey: 'r_ice', max: 130, min: 13, label: 'Cloud ice particle radius (10^-6 m)'},
+    {nonSurfaceKey: 'tauaer_sw', max: 3, label: 'Aerosol SW optical depth'},
+    {nonSurfaceKey: 'ssaaer_sw', max: 1, label: 'Aerosol SW s.s. albedo'},
+    {nonSurfaceKey: 'asmaer_sw', max: 1, min: -1, label: 'Aerosol SW asymm. scattering'},
+    {nonSurfaceKey: 'tauaer_lw', max: 3, label: 'Aerosol LW optical depth'}
+]
+
+var checkedList = $.grep(inputList, function(el,ind) { return el.on})
 
 var mouseDown = false
 var changed = false
@@ -336,37 +363,42 @@ initializeInput = function() {
     //     .attr("x2", altitudeAxis + inputWidth + outputWidth)
     //     .attr("y2", flowHeight)
 
-    list = [
-        {nonSurfaceKey: 'Tbound', surfaceKey: 'Ts', min: 150, max: 350, label: 'Temperature (K)'},
-        {nonSurfaceKey: 'rh', max: 100., label: 'relative humidity (%)'},
-        {nonSurfaceKey: 'co2', max: 3000, label: 'CO2 (ppm)'},
-        {nonSurfaceKey: 'ch4', max: 10000, label: 'CH4 (ppb)'},
-        {nonSurfaceKey: 'n2o', max: 1000, label: 'N2O (ppb)'},
-        {nonSurfaceKey: 'o3', max: 50, label: 'O3 (ppm)'},
-        {nonSurfaceKey: 'cfc11', max: 1000, label: 'CFC-11 (ppt)'},
-        {nonSurfaceKey: 'cfc12', max: 1000, label: 'CFC-12 (ppt)'},
-        {nonSurfaceKey: 'cfc22', max: 1000, label: 'CFC-22 (ppt)'},
-        {nonSurfaceKey: 'ccl4', max: 50, label: 'CCl4 (ppt)'},
-        {nonSurfaceKey: 'cldf', max: 1, label: 'Cloud fraction'},
-        {nonSurfaceKey: 'clwp', max: 30, label: 'In-cloud liquid water path (g/m2)'},
-        {nonSurfaceKey: 'r_liq', max: 100, label: 'Cloud water drop radius (10^-6 m)'},
-        {nonSurfaceKey: 'ciwp', max: 30, label: 'In-cloud ice water path (g/m2)'},
-        {nonSurfaceKey: 'r_ice', max: 130, min: 13, label: 'Cloud ice particle radius (10^-6 m)'},
-        {nonSurfaceKey: 'tauaer_sw', max: 3, label: 'Aerosol SW optical depth'},
-        {nonSurfaceKey: 'ssaaer_sw', max: 1, label: 'Aerosol SW s.s. albedo'},
-        {nonSurfaceKey: 'asmaer_sw', max: 1, min: -1, label: 'Aerosol SW asymm. scattering'},
-        {nonSurfaceKey: 'tauaer_lw', max: 3, label: 'Aerosol LW optical depth'}
-    ]
+
+    
+    // Make checkboxes
+    text = ''
+    inputList.map(function(args, index) {
+        if (args.double != 2) { text += '<li>' }
+        text += '<input class="profileCheckbox" type="checkbox" ' + (args.on ? 'checked = "checked"' : '') + 'id="' + args.nonSurfaceKey + '" /><label for="' + args.nonSurfaceKey + '" >' + args.label.replace(/\s\(.+\)/, "") + '</label>'
+        if (args.double != 1) { text += '</li>'}
+    })
+    $('ul.control').append(text)
+    
+    $('input.profileCheckbox').change(function(){
+        if ($(this).prop('checked')) {
+            var nonSurfaceKey = $(this).attr('id')
+            checkedList = checkedList.concat($.grep(inputList, function(el, ind) {return el.nonSurfaceKey == nonSurfaceKey}))
+        } else {
+            var nonSurfaceKey = $(this).attr('id')
+            checkedList = $.grep(checkedList, function(el, ind) {return el.nonSurfaceKey != nonSurfaceKey})
+        }
+        $('g.profileGroup').remove()
+        initializeProfiles()
+    })
+    initializeProfiles()
+}
+
+initializeProfiles = function() {
+    var y = d3.scale.linear().domain([0, d3.max(modelData['altitude'])]).range([flowHeight, 0])
     
     var vis = d3.select('svg#inner')
-    
-    
     $('svg#inner')
-        .attr('width', (list.length * profileWidth + (list.length - 1) * subsectionMargin) + 'px')
-
-    list.map(function(args, i) {
+        .attr('width', ((checkedList.length + 1) * profileWidth + checkedList.length * subsectionMargin) + 'px')
+    $('.control').attr('width', profileWidth).attr('height', flowHeight)
+    checkedList.map(function(args, index) {
+        var i = index + 1
         var g = vis.append('svg:g')
-                .attr('class', args.nonSurfaceKey)
+                .attr('class', 'profileGroup ' + args.nonSurfaceKey)
 
         g.append('line')
         .attr('x1', (profileWidth + subsectionMargin) * i)
@@ -436,8 +468,8 @@ initializeInput = function() {
     })
     vis.on("mousemove", function(d,j) {
         if ((mouseDown && (d3.mouse(this)[1] < flowHeight)) && (d3.mouse(this)[0] % (profileWidth + subsectionMargin) < profileWidth)) {
-            var xindex = Math.floor(d3.mouse(this)[0] / (profileWidth + subsectionMargin))
-            args = list[xindex]
+            var xindex = Math.floor(d3.mouse(this)[0] / (profileWidth + subsectionMargin)) - 1
+            args = checkedList[xindex]
             
             var g = d3.select('g.' + args.nonSurfaceKey)
             
@@ -455,6 +487,7 @@ initializeInput = function() {
                 g.select('circle').attr('cx', args.x(args.values[0]))
             }
             args.profile.remove()
+            
             args['profile'] = g.append('svg:path').attr('class', 'profile').attr('d', args.line(args.values))
             if ((args.surfaceKey) && (ci == 0)) {
                 modelData[args.surfaceKey] = newValue
